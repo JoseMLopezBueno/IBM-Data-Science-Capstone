2526de598ebeb92faba1021817a3570d96fa441a
Jose M Lopez Bueno <jlopezbueno@yahoo.ca>
2020-05-07 21:34:54 -0500

    Update Week1Assignment1_JoseLopez.py

--- Week1Assignment1_JoseLopez.py
+++ Week1Assignment1_JoseLopez.py
@@ -6,25 +6,17 @@
 # <h2>Week 1 - Assignment 1</h2>
 
 # In[3]:
-
-
 import pandas as pd
 import numpy as np
 
 
 # In[4]:
 
-
 print ('Hello Capstone Project Course!')
 
 
 # In[ ]:
 
-
-
-
-
-# In[ ]:
 
 
 
