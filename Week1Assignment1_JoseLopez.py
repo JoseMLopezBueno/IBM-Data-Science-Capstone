defde1969da1d1a62fdb27d62edf63c4d9c32fa6
Jose M Lopez Bueno <jlopezbueno@yahoo.ca>
2020-05-07 21:34:06 -0500

    Add files via upload

---
+++ Week1Assignment1_JoseLopez.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Applied Data Science Capstone | Jose Lopez Bueno</h1>
+
+# <h2>Week 1 - Assignment 1</h2>
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print ('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
